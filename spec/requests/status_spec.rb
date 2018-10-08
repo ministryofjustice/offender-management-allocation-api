40796e12a08af453760dbd1c5dab55375bceb6e4
Toby Retallick <toby.retallick@gmail.com>
2018-10-08 15:47:31 +0100

    Create 'status' endpoint
    - returns 'ok' in JSON
    - request spec added

---
+++ spec/requests/status_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Status requests' do
+  describe 'GET /status' do
+    it 'returns a status message' do
+      get('/status')
+      json = JSON.parse(response.body)
+
+      expect(json['status']).to eq('ok')
+    end
+  end
+end
