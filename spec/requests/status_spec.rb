16575b8ac7fc75a67def4223be83af5c9794f909
Toby Retallick <toby.retallick@gmail.com>
2018-10-09 10:01:07 +0100

    Only have a partial regex match for postrgres version in status request spec

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -10,12 +10,10 @@ RSpec.describe 'Status requests' do
     end
 
     it 'returns the postgres client version' do
-      postgres_version = 'PostgreSQL 10.3 on x86_64-apple-darwin14.5.0, compiled by Apple LLVM version 7.0.0 (clang-700.1.76), 64-bit'
-
       get('/status')
       json = JSON.parse(response.body)
 
-      expect(json['postgresVersion']).to eq(postgres_version)
+      expect(json['postgresVersion']).to match(/PostgreSQL/)
     end
   end
 end
