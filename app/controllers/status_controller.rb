4e6f017f23fd6b460cd757c2aa3fb9077d7708df
Toby Retallick <toby.retallick@gmail.com>
2018-10-09 16:24:24 +0100

    Fix Rubocop errors

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,9 +2,9 @@ class StatusController < ApplicationController
   def index
     postgres_version = ActiveRecord::Base.connection.select_value('SELECT version()')
 
-    render(json: { 
-      'status' => 'ok',
-      'postgresVersion' => postgres_version
-    })
+    render(json: {
+             'status' => 'ok',
+             'postgresVersion' => postgres_version
+           })
   end
 end
