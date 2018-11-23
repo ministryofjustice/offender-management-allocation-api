f1a1880bbc62aa41040200f1272d0603b865e430
Toby Retallick <toby.retallick@gmail.com>
2018-11-23 09:58:00 +0000

    Create implementation and tests for API endpoint authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+require 'jwt'
 class ApplicationController < ActionController::API
+  def authenticate
+    head 401 unless token && token_valid?
+  end
+
+private
+
+  def token
+    request.headers[:authorization]&.split(' ')&.last
+  end
+
+  def token_valid?
+    JWT.decode token,
+      nomis_oauth_public_key,
+      true,
+      algorithm: 'RS256'
+  rescue JWT::ExpiredSignature, JWT::DecodeError
+    head 401
+  end
+
+  def nomis_oauth_public_key
+    OpenSSL::PKey::RSA.new(Rails.configuration.nomis_oauth_public_key)
+  end
 end
---
+++ spec/requests/application_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+describe ApplicationController do
+  describe 'JWT validation' do
+    describe 'without a JWT token' do
+      it 'returns HTTP status 401' do
+        get('/status')
+
+        expect(response.status).to be(401)
+      end
+    end
+
+    describe 'with a valid JWT token' do
+      it 'returns HTTP status 200' do
+        get('/status', headers: generate_jwt_token)
+
+        expect(response.status).to be(200)
+      end
+    end
+
+    describe 'with an invalid JWT token' do
+      describe 'that is expired' do
+        it 'returns HTTP status 401' do
+          expired_token = generate_jwt_token(exp: Time.new(2018).to_i)
+
+          get('/status', headers: expired_token)
+
+          expect(response.status).to be(401)
+        end
+      end
+
+      describe 'that has been encoded with an incorrect or altered private key' do
+        it 'returns HTTP status 401' do
+          Rails.configuration.nomis_oauth_public_key = OpenSSL::PKey::RSA.generate(2048).public_key
+          payload = {}
+          incorrect_token = OpenSSL::PKey::RSA.generate(2048)
+          token = JWT.encode payload, incorrect_token, 'RS256'
+          token = { 'Authorization': token.to_s }
+
+          get('/status', headers: token)
+
+          expect(response.status).to be(401)
+        end
+      end
+    end
+  end
+end
