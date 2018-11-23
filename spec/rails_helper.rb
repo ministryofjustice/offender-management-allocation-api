7762a4f01bd3a3832d8906ed1740746690aed76a
Toby Retallick <toby.retallick@gmail.com>
2018-11-23 09:59:09 +0000

    Refactoring - create spec helpers for JSON Web Token creation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/helpers/request_helper'
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -14,6 +16,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.include Request::AuthHelpers, type: :request
 
   config.before(:each, :expect_exception) do
     Rails.configuration.sentry_dsn = 'https://test.com'
---
+++ spec/support/helpers/request_helper.rb
@@ -0,0 +1,22 @@
+module Request
+  module AuthHelpers
+    def generate_jwt_token(options = {})
+      payload = {
+        'internal_user': false,
+        'scope': %w[read write],
+        'exp': Time.new.to_i + 4 * 3600,
+        'client_id': 'offender-manager-allocation-manager'
+      }.merge(options)
+
+      rsa_private = OpenSSL::PKey::RSA.generate 2048
+      rsa_public = rsa_private.public_key
+
+      Rails.configuration.nomis_oauth_public_key = rsa_public
+
+      token = JWT.encode payload, rsa_private, 'RS256'
+      {
+        'Authorization': token.to_s
+      }
+    end
+  end
+end
